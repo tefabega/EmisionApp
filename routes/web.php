c5d2a59ba50b72e13a2b6373103bbef110eb1f90
tefabega <stefanybedoyagarcia@gmail.com>
2021-10-28 17:43:17 -0500

    Instalacion de Layout

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Auth::routes();
+Route::get('/', [App\Http\Controllers\LandingController::class, 'index'])->name('/');
+
+
+/*Auth::routes();
 
 /*asi se definen las rutas */
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
